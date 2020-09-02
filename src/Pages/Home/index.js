d6a0cbbeb6ea29cfc1f80643e95d086bed97b1f5
Emerson Silva <emerson_jdss@hotmail.com>
2020-09-01 22:48:10 -0300

    change home area for tests

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -6,21 +6,17 @@ export default function Home() {
     <>
       <Container>
         <Jumbotron>
-          <h1 className="display-3">Hello, world!</h1>
-          <p className="lead">
-            This is a simple hero unit, a simple Jumbotron-style component for
-            calling extra attention to featured content or information.
-          </p>
+          <h1 className="display-3">Spectrum</h1>
+          <p className="lead">Autistas nas melhores vagas do mercado</p>
           <hr className="my-2" />
-          <p>
+          {/* <p>
             It uses utility classes for typography and spacing to space content
             out within the larger container.
-          </p>
+          </p> */}
           <p className="lead">
             <Button color="primary">Learn More</Button>
           </p>
         </Jumbotron>
-        <Button color="danger">Danger!</Button>
       </Container>
     </>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,7 +22,7 @@ const NavMenu = (props) => {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarBrand href="/">Spectrum</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
