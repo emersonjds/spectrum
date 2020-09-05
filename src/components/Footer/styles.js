b802767548eb37e9bb1110900578d59280495994
Emerson Silva <emerson_jdss@hotmail.com>
2020-09-05 07:53:45 -0300

    finish footer layout

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContainerSocial = styled.div``;
+
+export const LineSocial = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+`;
+
+export const ContainerContacts = styled.div``;
+
+export const LineContacts = styled.div``;
